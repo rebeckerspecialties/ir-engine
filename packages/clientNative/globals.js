d140503883c4a1716676af1282d67524a2e004e1
Henry Allen <hmallen99@gmail.com>
2024-11-21 09:35:10 -0500

    use in-memory localstorage

--- packages/clientNative/globals.js
+++ packages/clientNative/globals.js
@@ -29,3 +29,18 @@ import structuredClone from '@ungap/structured-clone';
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 global.structuredClone = structuredClone;
+global.localStorage = {
+  _data: {},
+  setItem: function (id, val) {
+    return (this._data[id] = String(val));
+  },
+  getItem: function (id) {
+    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+  },
+  removeItem: function (id) {
+    return delete this._data[id];
+  },
+  clear: function () {
+    return (this._data = {});
+  },
+};
