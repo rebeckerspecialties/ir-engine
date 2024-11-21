ec405aa4af7a76adad834123ca9451eb963af0dc
Henry Allen <hmallen99@gmail.com>
2024-11-21 11:27:52 -0500

    Fix polyfill

--- packages/clientNative/globals.js
+++ packages/clientNative/globals.js
@@ -35,7 +35,7 @@ global.localStorage = {
     return (this._data[id] = String(val));
   },
   getItem: function (id) {
-    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+    return this._data.hasOwnProperty(id) ? this._data[id] : null;
   },
   removeItem: function (id) {
     return delete this._data[id];
