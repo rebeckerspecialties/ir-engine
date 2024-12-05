e01ed09a18e69fc5f86af8a241bccdc84922e5fa
Henry Allen <hmallen99@gmail.com>
2024-12-05 16:28:04 -0500

    Add screenshot testing

--- packages/screenshot-testing/compareScreenshots.js
+++ packages/screenshot-testing/compareScreenshots.js
@@ -6,8 +6,8 @@ Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
 https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
 The License is based on the Mozilla Public License Version 1.1, but Sections 14
-and 15 have been added to cover use of software over a computer network and 
-provide for limited attribution for the Original Developer. In addition, 
+and 15 have been added to cover use of software over a computer network and
+provide for limited attribution for the Original Developer. In addition,
 Exhibit A has been modified to be consistent with Exhibit B.
 
 Software distributed under the License is distributed on an "AS IS" basis,
@@ -19,16 +19,16 @@ The Original Code is Infinite Reality Engine.
 The Original Developer is the Initial Developer. The Initial Developer of the
 Original Code is the Infinite Reality Engine team.
 
-All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023 
+All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023
 Infinite Reality Engine. All Rights Reserved.
 */
 
-
-const fs = require('fs')
+const { readFileSync, readdirSync } = require('fs')
+const path = require('path')
 
 function expectBitmapsToBeEqual(imagePath, expectedImagePath) {
-  const bitmapBuffer = fs.readFileSync(imagePath)
-  const expectedBitmapBuffer = fs.readFileSync(expectedImagePath)
+  const bitmapBuffer = readFileSync(imagePath)
+  const expectedBitmapBuffer = readFileSync(expectedImagePath)
   if (!bitmapBuffer.equals(expectedBitmapBuffer)) {
     throw new Error(
       `Expected image at ${imagePath} to be equal to image at ${expectedImagePath}, but it was different!`
@@ -36,5 +36,18 @@ function expectBitmapsToBeEqual(imagePath, expectedImagePath) {
   }
 }
 
-// TODO: set correct paths
-expectBitmapsToBeEqual('./expectedStartupScreenshot.png', './startupScreenshot.png')
+function screenshotTest() {
+  const screenshotsPath = path.resolve(process.cwd(), 'screenshots')
+  const expectedScreenshotsPath = path.resolve(process.cwd(), 'expectedScreenshots')
+  const files = readdirSync(screenshotsPath, {
+    recursive: true
+  })
+
+  for (const file of files) {
+    const screenshot = screenshotsPath + '/' + file
+    const expectedScreenshot = expectedScreenshotsPath + '/' + file
+    expectBitmapsToBeEqual(expectedScreenshot, screenshot)
+  }
+}
+
+screenshotTest()
