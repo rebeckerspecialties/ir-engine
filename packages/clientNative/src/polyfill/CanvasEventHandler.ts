b8343499853e283b3eebade5ca90b2342c5cc6a6
Henry Allen <hmallen99@gmail.com>
2024-12-10 16:50:28 -0500

    EventHandler stub

--- packages/clientNative/src/polyfill/CanvasEventHandler.ts
+++ packages/clientNative/src/polyfill/CanvasEventHandler.ts
@@ -6,8 +6,8 @@ Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
 https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
 The License is based on the Mozilla Public License Version 1.1, but Sections 14
-and 15 have been added to cover use of software over a computer network and 
-provide for limited attribution for the Original Developer. In addition, 
+and 15 have been added to cover use of software over a computer network and
+provide for limited attribution for the Original Developer. In addition,
 Exhibit A has been modified to be consistent with Exhibit B.
 
 Software distributed under the License is distributed on an "AS IS" basis,
@@ -19,17 +19,33 @@ The Original Code is Infinite Reality Engine.
 The Original Developer is the Initial Developer. The Initial Developer of the
 Original Code is the Infinite Reality Engine team.
 
-All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023 
+All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023
 Infinite Reality Engine. All Rights Reserved.
 */
 
 import {PanResponderCallbacks} from 'react-native';
 
+type DomEventHandler = (evt: Event) => void;
+
 export function createCanvasEventHandler() {
-  const addEventListener = () => {};
+  const listenerRegistry: Map<string, Set<DomEventHandler>> = new Map();
 
-  const removeEventListener = () => {};
+  const addEventListener = (type: string, handler: DomEventHandler) => {
+    let registry = listenerRegistry.get(type);
+    if (!registry) {
+      registry = new Set<DomEventHandler>();
+      listenerRegistry.set(type, registry);
+    }
+    registry.add(handler);
+  };
+  const removeEventListener = (type: string, handler: DomEventHandler) => {
+    const registry = listenerRegistry.get(type);
+    if (registry) {
+      registry.delete(handler);
+    }
+  };
 
+  // TODO: add the correct variant of listenerRegistry.get(type).forEach((handler) => handler(evt))
   const panResponder: PanResponderCallbacks = {
     // Ask to be the responder:
     onStartShouldSetPanResponder: (evt, gestureState) => true,
@@ -65,6 +81,6 @@ export function createCanvasEventHandler() {
 
   return {
     eventListener: {addEventListener, removeEventListener},
-    panResponder: {},
+    panResponder,
   };
 }
