f929cc52e5a62a4df3a84c151dbb295d75e64311
Henry Allen <hmallen99@gmail.com>
2025-01-13 11:25:25 -0500

    Add todos

--- packages/engine/src/gltf/GLTFComponent.tsx
+++ packages/engine/src/gltf/GLTFComponent.tsx
@@ -6,8 +6,8 @@ Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
 https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
 The License is based on the Mozilla Public License Version 1.1, but Sections 14
-and 15 have been added to cover use of software over a computer network and 
-provide for limited attribution for the Original Developer. In addition, 
+and 15 have been added to cover use of software over a computer network and
+provide for limited attribution for the Original Developer. In addition,
 Exhibit A has been modified to be consistent with Exhibit B.
 
 Software distributed under the License is distributed on an "AS IS" basis,
@@ -19,7 +19,7 @@ The Original Code is Infinite Reality Engine.
 The Original Developer is the Initial Developer. The Initial Developer of the
 Original Code is the Infinite Reality Engine team.
 
-All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023 
+All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023
 Infinite Reality Engine. All Rights Reserved.
 */
 
@@ -189,6 +189,7 @@ export const GLTFComponent = defineComponent({
 
     const dependencies = gltfComponent.dependencies
     const progress = gltfComponent.progress.value
+    console.log(instanceID, progress)
     return componentDependenciesLoaded(dependencies.value as ComponentDependencies | undefined) && progress === 100
   },
 
@@ -263,6 +264,8 @@ const ResourceReactor = (props: { documentID: string; entity: Entity }) => {
     if (getComponent(props.entity, GLTFComponent).progress === 100) return
     if (!getState(GLTFDocumentState)[props.documentID]) return
     const entities = resourceQuery.filter((e) => getComponent(e, SourceComponent) === props.documentID)
+    // TODO: why isn't avatar dependency loaded?
+    console.log(props, entities, dependenciesLoaded, getState(GLTFDocumentState)[props.documentID])
     if (!entities.length) {
       if (dependenciesLoaded) {
         getMutableComponent(props.entity, GLTFComponent).progress.set(100)
