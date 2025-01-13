1093d1b614ce5bb7367867b630151bdd780ecff3
Henry Allen <hmallen99@gmail.com>
2025-01-13 11:22:03 -0500

    Start implementing KTX2 loader

--- packages/engine/src/assets/loaders/gltf/KTX2Loader.native.ts
+++ packages/engine/src/assets/loaders/gltf/KTX2Loader.native.ts
@@ -6,8 +6,8 @@ Version 1.0. (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
 https://github.com/ir-engine/ir-engine/blob/dev/LICENSE.
 The License is based on the Mozilla Public License Version 1.1, but Sections 14
-and 15 have been added to cover use of software over a computer network and 
-provide for limited attribution for the Original Developer. In addition, 
+and 15 have been added to cover use of software over a computer network and
+provide for limited attribution for the Original Developer. In addition,
 Exhibit A has been modified to be consistent with Exhibit B.
 
 Software distributed under the License is distributed on an "AS IS" basis,
@@ -19,13 +19,16 @@ The Original Code is Infinite Reality Engine.
 The Original Developer is the Initial Developer. The Initial Developer of the
 Original Code is the Infinite Reality Engine team.
 
-All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023 
+All portions of the code written by the Infinite Reality Engine team are Copyright © 2021-2023
 Infinite Reality Engine. All Rights Reserved.
 */
 
-import { CompressedTexture, CompressedTextureLoader, LoadingManager, WebGLRenderer } from 'three'
+import { read } from '@callstack/react-native-basis-universal'
+import { CompressedTexture, LoadingManager, WebGLRenderer } from 'three'
+import { FileLoader } from '../base/FileLoader'
+import { Loader } from '../base/Loader'
 
-export class KTX2Loader extends CompressedTextureLoader {
+export class KTX2Loader extends Loader {
   constructor(manager?: LoadingManager) {
     super(manager)
     console.warn('NATIVE KTX2 LOADER IS INITIALIZED')
@@ -45,23 +48,54 @@ export class KTX2Loader extends CompressedTextureLoader {
     return this
   }
 
-  parse(
-    buffer: ArrayBuffer,
-    onLoad: (texture: CompressedTexture) => void,
-    onError?: (event: ErrorEvent) => void
-  ): KTX2Loader {
-    debugger
-    return this
-  }
-
   load(
     url: string,
     onLoad: (texture: CompressedTexture) => void,
     onProgress?: (requrest: ProgressEvent<EventTarget>) => void | undefined,
     onError?: ((event: ErrorEvent) => void) | undefined,
     signal?: AbortSignal
-  ): CompressedTexture {
-    debugger
-    return null
+  ) {
+    const loader = new FileLoader(this.manager)
+
+    loader.setResponseType('arraybuffer')
+    loader.setWithCredentials(this.withCredentials)
+
+    loader.load(
+      url,
+      (buffer) => {
+        this._createTexture(buffer)
+          .then((texture) => (onLoad ? onLoad(texture) : null))
+          .catch(onError)
+      },
+      onProgress,
+      onError,
+      signal
+    )
+  }
+
+  /**
+   * @param {ArrayBuffer} buffer
+   * @param {object?} config
+   * @return {Promise<CompressedTexture|CompressedArrayTexture|DataTexture|Data3DTexture>}
+   */
+  async _createTexture(buffer, config = {}) {
+    const container = read(new Uint8Array(buffer))
+
+    if (container.vkFormat !== VK_FORMAT_UNDEFINED) {
+      return createRawTexture(container)
+    }
+
+    //
+    const taskConfig = config
+    const texturePending = this.init()
+      .then(() => {
+        return this.workerPool.postMessage({ type: 'transcode', buffer, taskConfig: taskConfig }, [buffer])
+      })
+      .then((e) => this._createTextureFrom(e.data, container))
+
+    // Cache the task result.
+    _taskCache.set(buffer, { promise: texturePending })
+
+    return texturePending
   }
 }
